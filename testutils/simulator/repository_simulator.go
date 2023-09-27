fc8864152211556c4529f9b664f80edffbf05c25
Ivana Atanasova <iyovcheva@vmware.com>
2023-09-27 15:25:24 +0300

    Fix failing tests
    This change provides a fix for the failing CI due to logging
    updates, as well as updater changes in main
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

--- testutils/simulator/repository_simulator.go
+++ testutils/simulator/repository_simulator.go
@@ -286,7 +286,7 @@ func partition(s string, delimiter string) (string, string) {
 	return version, role
 }
 
-func (rs *RepositorySimulator) DownloadFile(urlPath string, maxLength int64) ([]byte, error) {
+func (rs *RepositorySimulator) DownloadFile(urlPath string, maxLength int64, timeout time.Duration) ([]byte, error) {
 	data, err := rs.fetch(urlPath)
 	if err != nil {
 		return data, err
